9a82cbbdde5d7b7f02196120c4e3da2f5088fb1f
Maochuan <thinkermao@outlook.com>
2016-08-06 16:59:05 +0800

    mem2reg pass

---
+++ script/Optimize/Mem2Reg.cpp
@@ -0,0 +1,15 @@
+#include "Mem2Reg.h"
+
+namespace script
+{
+
+    Mem2Reg::Mem2Reg()
+    {
+    }
+
+
+    Mem2Reg::~Mem2Reg()
+    {
+    }
+
+}
---
+++ script/Optimize/Mem2Reg.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Optimize.h"
+
+namespace script
+{
+    class Mem2Reg
+    {
+    public:
+        Mem2Reg();
+        virtual ~Mem2Reg();
+    };
+}
+
