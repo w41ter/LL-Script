25c0e3e5b574627370bd07e0d5f74e9d97917bb1
Maochuan <thinkermao@outlook.com>
2016-08-20 16:33:13 +0800

    change symbol table

--- script/IR/SymbolTable.h
+++ script/IR/SymbolTable.h
@@ -29,22 +29,14 @@ namespace script
         unsigned find(std::string &name, Symbol &symbol);
         void setCatched(std::string &name);
 
-        /*void insertDefines(std::string &str, Token &token);
-        void insertVariables(std::string &str, Token &token);
+        typedef std::map<std::string, Symbol>::iterator iterator;
+        iterator begin() { return table_.begin(); }
+        iterator end() { return table_.end(); }
 
-        unsigned findName(std::string &str) const;
-
-        Token getToken(std::string &str) const;*/
-        //ir::Value *getValue(std::string &str);
         SymbolTable *getParent() { return parent_; }
-        //void catchedName(std::string &str);
 
     protected:
         SymbolTable * parent_;
-        std::set<std::string> catched_;
         std::map<std::string, Symbol> table_;
-        std::map<std::string, Token> defines_;
-        std::map<std::string, Token> variables_;
-        //std::map<std::string, ir::Value*> values_;
     };
 }
